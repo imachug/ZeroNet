2a41b6745b892f03e0c0bfeaae38f02c4766b3a9
Ivanq <imachug@yandex.ru>
2019-10-28 00:01:26 +0300

    Make the extension Chrome-compatible

--- plugins/BootstrapUI/webext/proxy.js
+++ plugins/BootstrapUI/webext/proxy.js
@@ -1,15 +1,37 @@
-browser.proxy.onRequest.addListener(req => {
-	return {
-		type: "http",
-		host: "127.0.0.1",
-		port: 43110
-	};
-}, {
-	urls: ["*://*.zeronet/*"]
-});
+// Set proxy
+if(globalThis.browser) {
+	// Firefox-compatible
+	browser.proxy.onRequest.addListener(req => {
+		return {
+			type: "http",
+			host: "127.0.0.1",
+			port: 43110
+		};
+	}, {
+		urls: ["*://*.zeronet/*"]
+	});
+} else {
+	// Chrome-like
+	chrome.proxy.settings.set({value: {
+		mode: "pac_script",
+		pacScript: {
+			data: `
+				function FindProxyForURL(url, host) {
+					if(host.endsWith(".zeronet")) {
+						return "PROXY 127.0.0.1:43110";
+					}
+					return "DIRECT";
+				}
+			`
+		}
+	}});
+}
 
 
-browser.webRequest.onBeforeRequest.addListener(req => {
+
+const browserChrome = globalThis.browser || globalThis.chrome;
+
+browserChrome.webRequest.onBeforeRequest.addListener(req => {
 	return {
 		redirectUrl: "http://home.zeronet/ZeroNet-Internal/Index"
 	};
@@ -17,28 +39,39 @@ browser.webRequest.onBeforeRequest.addListener(req => {
 	urls: ["*://*/zeronet-bootstrap"]
 }, ["blocking"]);
 
-
 // Redirect all currently opened zeronet-bootstrap pages
 (async () => {
-	for(const tab of await browser.tabs.query({})) {
+	let tabs;
+	if(globalThis.browser) {
+		tabs = await browser.tabs.query({});
+	} else {
+		tabs = await new Promise(resolve => chrome.tabs.query({}, resolve));
+	}
+	for(const tab of tabs) {
 		if(tab.url.endsWith("/zeronet-bootstrap")) {
-			browser.tabs.update(tab.id, {
-				url: "http://home.zeronet/ZeroNet-Internal/Index",
-				loadReplace: true
-			});
+			if(globalThis.browser) {
+				browser.tabs.update(tab.id, {
+					url: "http://home.zeronet/ZeroNet-Internal/Index",
+					loadReplace: true
+				});
+			} else {
+				chrome.tabs.update(tab.id, {
+					url: "http://home.zeronet/ZeroNet-Internal/Index"
+				});
+			}
 		}
 	}
 })();
 
 
 async function updateIcon(tabId) {
-	const tab = await new Promise(resolve => browser.tabs.get(tabId, resolve));
+	const tab = await new Promise(resolve => browserChrome.tabs.get(tabId, resolve));
 	const isZeroNet = /^(.*?):\/\/[^\/]+\.zeronet/.test(tab.url);
-	browser.browserAction.setIcon({
+	browserChrome.browserAction.setIcon({
 		tabId: tabId,
 		path: isZeroNet ? "logo.png" : "logo-inactive.png"
 	});
 }
 
-browser.tabs.onCreated.addListener(tab => updateIcon(tab.id));
-browser.tabs.onUpdated.addListener(updateIcon);
+browserChrome.tabs.onCreated.addListener(tab => updateIcon(tab.id));
+browserChrome.tabs.onUpdated.addListener(updateIcon);
